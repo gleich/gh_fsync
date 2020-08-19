fc2c7126d5d7ae8024b8334150c2bdb83a0e845b
Matthew Gleich <matthewgleich@gmail.com>
2020-08-19 19:26:51 -0400

    🐛 Log error and exit with 1
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- internal/changes/commit.go
+++ internal/changes/commit.go
@@ -1,6 +1,8 @@
 package changes
 
 import (
+	"os"
+
 	"github.com/Matt-Gleich/logoru"
 	"github.com/go-git/go-git/v5"
 )
@@ -10,19 +12,21 @@ func Commit() git.Repository {
 	repo, err := git.PlainOpen(".")
 	if err != nil {
 		logoru.Error("Failed to open git repo", err)
+		os.Exit(1)
 	}
 	tree, err := repo.Worktree()
 	if err != nil {
 		logoru.Error("Failed to get working tree", err)
+		os.Exit(1)
 	}
 
 	_, err = tree.Commit("", &git.CommitOptions{
 		All: true,
 	})
 	if err != nil {
-		logoru.Error("Failed to commit changes")
+		logoru.Error("Failed to commit changes", err)
+		os.Exit(1)
 	}
 	logoru.Success("Committed changes!")
-
 	return *repo
 }
