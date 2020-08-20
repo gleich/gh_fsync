2b62896bb3c27c027164bee478c91dc24bee739b
Matthew Gleich <matthewgleich@gmail.com>
2020-08-20 14:22:36 -0400

    🐛 Output raw stdout with no new lines
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- pkg/utils/command.go
+++ pkg/utils/command.go
@@ -11,7 +11,7 @@ func RunCommand(command string, msg string) string {
 	cmdChunks := strings.Split(command, " ")
 	out, err := exec.Command(cmdChunks[0], cmdChunks[1:]...).Output()
 	strOut := string(out)
-	fmt.Println(strOut)
+	fmt.Print(strOut)
 	CheckErr(msg, err)
 	return strOut
 }
