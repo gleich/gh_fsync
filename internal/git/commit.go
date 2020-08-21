b46697a86b2c5b1f0a32c93c01e6c10926b3c6d4
Matthew Gleich <matthewgleich@gmail.com>
2020-08-21 00:20:28 -0400

    🐛 Remove quotes from commit
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- internal/git/commit.go
+++ internal/git/commit.go
@@ -17,7 +17,7 @@ func Commit(config config.Outline) {
 	utils.RunCommand("git", []string{"config", "--global", "user.name", "\"Publishing Bot\""}, "Failed to config user name")
 	utils.RunCommand("git", []string{"add", "."}, "Failed to stage changes")
 	utils.RunCommand("git", []string{"status"}, "Failed to get git status")
-	utils.RunCommand("git", []string{"commit", "-m", "\"" + commitMsg + "\""}, "Failed to commit changes")
+	utils.RunCommand("git", []string{"commit", "-m", commitMsg}, "Failed to commit changes")
 	utils.RunCommand("git", []string{"push"}, "Failed to push changes")
 	logoru.Success("Committed changes!")
 }
